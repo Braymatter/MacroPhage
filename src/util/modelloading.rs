ad094d1cd67bf783910e6cd0c1cad63082bce61c
Matthew <logicprojectsforfpgas@gmail.com>
2022-08-06 21:04:53 -0600

    Color changing hierarchy hell

--- src/util/modelloading.rs
+++ src/util/modelloading.rs
@@ -25,6 +25,7 @@ impl Plugin for ModelPlugin {
             //ProgressPlugin::new(LoadingStates::AssetLoading).continue_to(LoadingStates::Loaded),
             //)
             //.add_system(model_loading_progress)
+            .add_system(model_coloring)
             .add_startup_system(load_gltfs);
     }
 }
@@ -60,6 +61,36 @@ pub struct NodeTenentAssets {
     //This one is cursed, something wrong with the gltf file
     //#[asset(path = "3DArt/Cell Variations/Cell Var 3.gltf#Scene0")]
     //cell_var_3: Handle<Scene>,
+}
+
+pub fn model_coloring(
+    nodes: Query<(&crate::game::Node, &Parent)>,
+    parents: Query<&Children>,
+    mut mat_handles: Query<&mut Handle<StandardMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    for (node, parent) in &nodes {
+        let color = node.force.color();
+        //If I am your parent then I must have children
+        let children = parents.get(parent.get()).unwrap();
+        //Crawl through children until you find one with the material
+        let mut children: Vec<&Entity> = children.iter().collect();
+        while !children.is_empty() {
+            //Children is not empty
+            let checking = children.pop().unwrap();
+            if let Ok(mut material) = mat_handles.get_mut(*checking) {
+                // This is the child
+                let mut mat = materials.get(&material).unwrap().clone();
+                mat.emissive = color;
+                *material = materials.add(mat);
+            }
+            //Add my children to the list to search
+            if let Ok(my_children) = parents.get(*checking) {
+                let mut my_children: Vec<&Entity> = my_children.iter().collect();
+                children.append(&mut my_children);
+            }
+        }
+    }
 }
 
 pub fn spawn_model(
@@ -98,7 +129,7 @@ fn load_gltfs(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(NodeTenentAssets {
         replicator: asset_server.load("3DArt/Replicator/Replicator.gltf#Scene0"),
         nexus: asset_server.load("3DArt/Nexus/Nexus.gltf#Scene0"),
-        cell: asset_server.load("3DArt/Cell/Cell.gltf#Scene0"),
+        cell: asset_server.load("3DArt/Cell/Cell.glb#Scene0"),
         cell_var_1: asset_server.load("3DArt/Cell Variations/Cell Var 1.gltf#Scene0"),
         cell_var_2: asset_server.load("3DArt/Cell Variations/Cell Var 2.gltf#Scene0"),
     });
