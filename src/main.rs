e4916166917b589207b77812fe36e6c18a09ea52
Joshua Powers <jp@joshuapowers.com>
2022-08-07 02:32:53 -0500

    added 4k motherboard texture Fethur created
    can use the old space texture instead if we like it better

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use macrophage::{
 use macrophage::ui::phage_select::PhageSelectPlugin;
 use macrophage::util::ui::CustomEguiStyle;
 use std::f32::consts::PI;
+use std::ops::{Add, Mul};
 
 pub const HEIGHT: f32 = 900.0;
 pub const RESOLUTION: f32 = 16.0 / 9.0;
@@ -85,16 +86,17 @@ fn create_background(
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
     let material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(asset_server.load("background.png")),
+        base_color_texture: Some(asset_server.load("background_alt_4k.png")),
         unlit: true,
         ..default()
     });
-    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(200., 200.))));
+    // adjust quad shape to affect zoom
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(64., 64.))));
     commands.spawn_bundle(PbrBundle {
         mesh: quad_handle,
         material: material_handle,
         transform: Transform::from_xyz(0., -20., 0.0)
-            .with_rotation(Quat::from_axis_angle(Vec3::X, -PI / 2.0)),
+            .with_rotation(Quat::from_axis_angle(Vec3::X, -PI / 2.0).mul(Quat::from_axis_angle(Vec3::Z, -PI / 2.0 ))),
         ..default()
     });
 }
