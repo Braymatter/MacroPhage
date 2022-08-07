1511761ac2196594ad22c23118a30cee02e84224
Cayle Bray <cayle.bray@braymatter.com>
2022-08-06 19:59:29 -0500

    ugggghhh

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,24 @@
+use std::net::IpAddr;
+
 use bevy::{log::LogPlugin, prelude::*};
 use bevy_renet::RenetServerPlugin;
 use macrophage::net::gamehost::GameHostPlugin;
 
-fn main() {
+pub struct IpRes{
+    pub public_ip: IpAddr
+}
+
+#[tokio::main]
+async fn main() {
     let mut app = App::new();
+
+    if let Some(ip) = public_ip::addr().await{
+        app.insert_resource(IpRes{public_ip: ip});
+        info!("Found Public Ip: {}", ip);
+    }else{
+        panic!("Could not fetch public ip, cannot mount server");
+    }
+
 
     app.add_plugins(MinimalPlugins);
     app.add_plugin(LogPlugin);
